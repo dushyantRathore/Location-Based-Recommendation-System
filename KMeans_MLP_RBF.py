d13e24c60f76caeccab507b288ea74bba151de81
Dushyant Rathore <dushyant.bgs@gmail.com>
2016-11-17 10:40:00 +0530

    Update KMeans_MLP_RBF.py

--- KMeans_MLP_RBF.py
+++ KMeans_MLP_RBF.py
@@ -6,89 +6,130 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import accuracy_score, recall_score, precision_score
 from sklearn.cluster import KMeans
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-f = pd.read_csv("/home/dushyant/Desktop/Minor/Codes/Foursquare Section/Cracked/UserID 15 - Original.csv", delimiter=",")
-df = pd.DataFrame(f)
+# Global Arrays
+acc_RBF = []
+prec_RBF = []
+rec_RBF = []
 
-Train, Test = train_test_split(df, test_size=0.3)
+# Global Learning Rate Array
+alpha = [0.01]
 
-cluster_size = 35
-cluster_array = np.zeros(cluster_size)
+for a in alpha:
 
-# Form the Training Set
-X_Train_DF = Train.ix[:, 2:5]
-X_Train = X_Train_DF.as_matrix()
-Y_Tr = Train["Y_Value"]
-Y_list1 = list(Y_Tr)
-Y_Train = np.asarray(Y_list1)
+    acc_Temp = []
+    prec_Temp = []
+    rec_Temp = []
 
-c = 0
-for j in Y_Train:
-    if j == 1:
-        c = c + 1
+    for i in range(1, 9):
 
-print "The no of 1's in Training Set : " + str(c)
+        x = "/home/dushyant/Desktop/Minor/Codes/Foursquare Section/Cracked/Dataset/user" + str(i) + ".csv"
+        f = pd.read_csv(x, delimiter=",")
+        df = pd.DataFrame(f)
 
-# Form the Testing Set
-X_Test_DF = Test.ix[:, 2:5]
-X_Test = X_Test_DF.as_matrix()
-Y_Te = Test["Y_Value"]
-Y_list2 = list(Y_Te)
-Y_Test = np.asarray(Y_list2)
+        Train, Test = train_test_split(df, test_size=0.3)
 
-kmeans = KMeans(n_clusters = cluster_size, random_state=0).fit(X_Train)
-print len(kmeans.labels_)
+        cluster_size = 35
+        cluster_array = np.zeros(cluster_size)
 
-pred = kmeans.predict(X_Train)
+        # Form the Training Set
+        X_Train_DF = Train.ix[:, 2:5]
+        X_Train = X_Train_DF.as_matrix()
+        Y_Tr = Train["Y_Value"]
+        Y_list1 = list(Y_Tr)
+        Y_Train = np.asarray(Y_list1)
 
-print "Prediction Array : "
-print pred
+        c = 0
+        for j in Y_Train:
+            if j == 1:
+                c = c + 1
 
-for i in range(0, len(pred)):
-    if Y_Train[i] == 1:
-        cluster_array[pred[i]] += 1
+        #print "The no of 1's in Training Set : " + str(c)
 
-print "Cluster Array : "
-print cluster_array
+        # Form the Testing Set
+        X_Test_DF = Test.ix[:, 2:5]
+        X_Test = X_Test_DF.as_matrix()
+        Y_Te = Test["Y_Value"]
+        Y_list2 = list(Y_Te)
+        Y_Test = np.asarray(Y_list2)
 
-m = max(cluster_array)
+        kmeans = KMeans(n_clusters=cluster_size, random_state=0).fit(X_Train)
+        #print len(kmeans.labels_)
 
-index = 0
-for i in range(0, cluster_size):
-    if cluster_array[i] == m:
-        index = i
+        pred = kmeans.predict(X_Train)
 
-print "Index Value : " + str(index)
+        #print "Prediction Array : "
+        #print pred
 
-# print cluster_array
+        for k in range(0, len(pred)):
+            if Y_Train[k] == 1:
+                cluster_array[pred[k]] += 1
 
-# MLP
-nn = Classifier(
-    layers=[
-        Layer("Sigmoid", units=100),
-        Layer("Softmax", name="OutputLayer", units=cluster_size)
-    ],
-    learning_rate=0.01,
-    n_iter=100
+        #print "Cluster Array : "
+        #print cluster_array
+
+        m = max(cluster_array)
+
+        index = 0
+        for l in range(0, cluster_size):
+            if cluster_array[l] == m:
+                index = l
+
+        #print "Index Value : " + str(index)
+
+        # MLP
+        nn = Classifier(
+            layers=[
+                Layer("Sigmoid", units=100),
+                Layer("Softmax", name="OutputLayer", units=cluster_size)
+            ],
+            learning_rate=a,
+            n_iter=100
+        )
+
+        nn.fit(X_Train, pred)
+        pred_RBF = nn.predict(X_Test)
+
+        for m in range(0, len(pred_RBF)):
+            if pred_RBF[m] == index:
+                pred_RBF[m] = 1
+            else:
+                pred_RBF[m] = 0
+
+        acc = accuracy_score(Y_Test, pred_RBF)
+        prec = precision_score(Y_Test, pred_RBF)
+        rec = recall_score(Y_Test, pred_RBF)
+
+        acc_Temp.append(acc * 100)
+        prec_Temp.append(prec)
+        rec_Temp.append(rec)
+
+    acc_RBF.append(sum(acc_Temp)/len(acc_Temp))
+    prec_RBF.append(sum(prec_Temp)/len(prec_Temp))
+    rec_RBF.append(sum(rec_Temp)/len(rec_Temp))
+
+print acc_RBF
+print prec_RBF
+print rec_RBF
+
+'''
+#Graph Plotting
+
+plotly.tools.set_credentials_file(username='dushyantRathore', api_key='5bzzn2vxpr')
+
+# Create a trace
+trace = go.Scatter(
+    x = alpha,
+    y = acc_RBF
 )
 
-nn.fit(X_Train, pred)
-pred_RBF = nn.predict(X_Test)
+data = [trace]
 
-for i in range(0, len(pred_RBF)):
-    if pred_RBF[i] == index:
-        pred_RBF[i] = 1
-    else:
-        pred_RBF[i] = 0
-
-acc = accuracy_score(Y_Test, pred_RBF)
-rec = recall_score(Y_Test, pred_RBF)
-prec = precision_score(Y_Test, pred_RBF)
-
-print "Accuracy : " + str(acc)
-print "Precision : " + str(prec)
-print "Recall : " + str(rec)
-
+# Plot and embed in ipython notebook!
+py.iplot(data, filename='line-MLP-RBF')'''
 
 
 
